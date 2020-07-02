fec88421846b4f54b3349563e75384ec26272083
Cleber <contato.cleberrezende@gmail.com>
2020-07-02 01:22:31 -0300

    Nova branch e middlewares/interceptors de api

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,26 @@ const api = axios.create({
     baseURL: 'http://localhost:3333',
 });
 
+api.interceptors.request.use(
+    (request) => {
+        // TODO
+        return request;
+    },
+    (error) => {
+        // TODO
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => {
+        // TODO
+        return response;
+    },
+    (error) => {
+        // TODO
+        return Promise.reject(error);
+    }
+);
+
 export default api;
